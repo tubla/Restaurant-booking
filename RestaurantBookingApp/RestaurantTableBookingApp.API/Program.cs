023e3f48a751ee07987dda8f8559105bc5e8d152
RAHUL26R <rahulroy.profile@gmail.com>
2023-08-31 17:27:08 +0530

    Configured Application Insights and Serilog for logging. Added an exception handler and a middleware to log the request and response httpcontext

--- RestaurantBookingApp/RestaurantTableBookingApp.API/Program.cs
+++ RestaurantBookingApp/RestaurantTableBookingApp.API/Program.cs
@@ -1,49 +1,103 @@
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using RestaurantBookingApp.Data;
 using RestaurantBookingApp.Service;
+using RestaurantTableBookingApp.API;
+using Serilog;
+using System.Net;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        // Configure serilog with the appsettings.json settings
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .MinimumLevel.Information()
+            .Enrich.FromLogContext()
+            .CreateBootstrapLogger();
 
-        // Add services to the container.
-
-        builder.Services.AddDbContext<RestaurantBookingDBContext>(options =>
+        try
         {
-            options.UseSqlServer(builder.Configuration.GetConnectionString("AzureDBConnectionString") ?? "")
-            .EnableSensitiveDataLogging(); // should not be used in production
-        });
+            var builder = WebApplication.CreateBuilder(args);
 
-        builder.Services.AddScoped<IRestaurantRepository, RestaurantRepository>(); // DI Configuration, one instance per http request, that means, if we need the instance in several places like in conroller then services, same instance copy would be shared in that request.
-        builder.Services.AddScoped<IRestaurantService, RestaurantService>(); // DI Configuration, one instance per http request, that means, if we need the instance in several places like in conroller then services, same instance copy would be shared in that request.
+            /* All details are available in https://github.com/serilog-contrib/serilog-sinks-applicationinsights */
+            builder.Services.AddApplicationInsightsTelemetry();
+            builder.Host.UseSerilog((context, services, loggerConfiguration) => loggerConfiguration
+                        .WriteTo.ApplicationInsights(
+                            services.GetRequiredService<TelemetryConfiguration>(),
+                                TelemetryConverter.Events));
 
-        /*
-        builder.Services.AddTransient<IRestaurantRepository, RestaurantRepository>(); // DI Configuration, every single time you inject/ask for instance, a new instance is given.
-        builder.Services.AddSingleton<IRestaurantRepository, RestaurantRepository>(); // DI Configuration, for the whole app only one instance would be created. It is danger to use as it is not thread safe.
-        */
+            // Add services to the container.
 
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<RestaurantBookingDBContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("AzureDBConnectionString") ?? "")
+                .EnableSensitiveDataLogging(); // should not be used in production
+            });
 
-        var app = builder.Build();
+            Log.Information("Starting the application....");
 
-        // Configure the HTTP request pipeline.
-        //if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            builder.Services.AddScoped<IRestaurantRepository, RestaurantRepository>(); // DI Configuration, one instance per http request, that means, if we need the instance in several places like in conroller then services, same instance copy would be shared in that request.
+            builder.Services.AddScoped<IRestaurantService, RestaurantService>(); // DI Configuration, one instance per http request, that means, if we need the instance in several places like in conroller then services, same instance copy would be shared in that request.
+
+            /*
+            builder.Services.AddTransient<IRestaurantRepository, RestaurantRepository>(); // DI Configuration, every single time you inject/ask for instance, a new instance is given.
+            builder.Services.AddSingleton<IRestaurantRepository, RestaurantRepository>(); // DI Configuration, for the whole app only one instance would be created. It is danger to use as it is not thread safe.
+            */
+
+
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            //Exception handling.
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+                    var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+                    var exception = exceptionHandlerPathFeature?.Error;
+
+                    Log.Error(exception, "Unhandled exception occurred. {ExceptionDetails}", exception?.ToString());
+
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    await context.Response.WriteAsync("An unexpected error occurred. Please try again later");
+                });
+            });
+
+            app.UseMiddleware<RequestResponseLoggingMiddleware>();
+
+
+
+            // Configure the HTTP request pipeline.
+            //if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
         }
+        catch (Exception ex)
+        {
 
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
+            Log.Fatal(ex, "Host terminated unexpectedly");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 }
---
+++ RestaurantBookingApp/RestaurantTableBookingApp.API/RequestResponseLoggingMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Serilog;
+
+namespace RestaurantTableBookingApp.API
+{
+    public class RequestResponseLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestResponseLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Log.Information($"Request : {context.Request.Method} {context.Request.Path}");
+
+            //copy the original response body stream
+            var originalBodyStream = context.Response.Body;
+
+            //create a new memory stream to capture the response
+            using (var responseBody = new MemoryStream())
+            {
+                //Set the response body stream to the memory stream
+                context.Response.Body = responseBody;
+
+                //continue processing the request
+                await _next(context);
+
+                //log the response
+                var response = await FormatResponse(context.Response);
+                Log.Information($"Response: {response}");
+
+                // Copy the captured response to the original response body stream
+                responseBody.Seek(0, SeekOrigin.Begin);
+                await responseBody.CopyToAsync(originalBodyStream);
+            }
+        }
+
+        private async Task<string> FormatResponse(HttpResponse response)
+        {
+            response.Body.Seek(0, SeekOrigin.Begin);
+            var text = await new StreamReader(response.Body).ReadToEndAsync();
+            response.Body.Seek(0, SeekOrigin.Begin);
+            return $"{response.StatusCode} : {text}";
+        }
+    }
+}
